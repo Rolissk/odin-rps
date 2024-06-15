64dc075128ecda32ac14371317b0c717bd759920
Rolissk <162630063+Rolissk@users.noreply.github.com>
2024-06-15 13:37:59 +0300

    Add functions for changing images, timer and popup
    Added function to change images of player and computer
    pictures when a choice is selected.
    
    Added countdown function which also disables buttons
    to prevent them from being pressed while round is being played.
    
    Added a modal to pop up when someone reaches 5 points.
    
    Added a restart button if the player is so bad that the computer
    beats him and player gets mad.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let humanScore = 0;
-let computerScore = 0;
-let humanChoice;
-let computerChoice;
-let pickPl;
-let pickPc;
+var humanScore = 0;
+var computerScore = 0;
+var humanChoice;
+var computerChoice;
+var pickPl;
+var pickPc;
+var playerPic = "playerPic"
+var compPic = "compPic"
+const resetPic = "questionMark"
+
 function getComputerChoice() { /*Assign a choice for computer*/
   let random = Math.floor(Math.random() * 3) + 1;
   if (random == 1) {
@@ -17,7 +21,7 @@ function getComputerChoice() { /*Assign a choice for computer*/
 }
 
 const btn = document.querySelector("#buttons");
-buttons.addEventListener("click", (event) => {
+btn.addEventListener("click", (event) => {
   let target = event.target;
 
   switch (target.id) {
@@ -41,47 +45,127 @@ buttons.addEventListener("click", (event) => {
   }
 })
 
+const resetBtn = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+  if (confirm("Are you sure you want to reset?") == true) {
+    newGameStart();
+  }
+});
 function playRound(humanPick, computerPick) {
+  disableButtons()
   if ((humanPick == "rock" && computerPick == "scissors") || (humanPick == "paper" && computerPick == "rock") ||
     (humanPick == "scissors" && computerPick == "paper")) {
-    changeImage(playerPic, humanChoice);/*sitaa funkcija*/
-    humanScore++;
-    updateScore();
-    /* pop up pazinojums par uzvaru - jauztaisa*/
-    if (humanScore === 5) {
-      alert(`You have won the game!`)
-      humanScore = 0;
-      computerScore = 0;
-      updateScore();
-    }
-    return humanScore;
+    changeImage(playerPic, humanChoice);
+    countDown(3, `humanScore`);
   } else if (humanPick === computerPick) {
-    /* pop up pazinojums par neizskirtu - jauztaisa*/
+    changeImage(playerPic, humanChoice);
+    countDown(3);
   } else {
-    computerScore++;
-    updateScore();
-    /* pop up pazinojums par zaudejumu - jauztaisa*/
-    if (computerScore === 5) {
-      alert(`Computer has won the game!`)
-      humanScore = 0;
-      computerScore = 0;
-      updateScore();
-    }
-    return computerScore;
+    changeImage(playerPic, humanChoice);
+    countDown(3, `computerScore`);
   }
 }
 
 function updateScore() {
   const playerScore = document.querySelector("#plScore");
-  playerScore.textContent = `You: ${humanScore}`
+  playerScore.textContent = `Player: ${humanScore}`
   const compScore = document.querySelector("#compScore");
   compScore.textContent = `Computer: ${computerScore}`
 }
 
-function changeImage(target, image) { /* seit taa funckija */
-  document.querySelector(`#${target}`).src = `./images/${image}.png`
+function changeImage(target, image) {
+  const imgElement = document.getElementById(target);
+  imgElement.setAttribute("src", `images/${image}.png`);
 }
-function countDown() {
-  /*When player picks his choice then fighting smoke effect covers the pick box and 
-  numbers on top of that image count down from 3 and on 0 picks and winner is revealed */
+function countDown(seconds, whoScored) {
+  let count = seconds
+  const interval = setInterval(() => {
+    const timer = document.querySelector("#countDown");
+    timer.textContent = `${count}`
+    count--;
+
+    if (count < 0) {
+      clearInterval(interval);
+      timer.textContent = "Fight!";
+      if (whoScored === `humanScore`) {
+        humanScore++;
+        updateScore();
+      } else if (whoScored === `computerScore`) {
+        computerScore++;
+        updateScore();
+      }
+      changeImage(compPic, computerChoice);
+      if (humanScore === 5) {
+        const winner = `Player`
+        winModal(winner)
+        newGameStart();
+      } else if (computerScore === 5) {
+        const winner = `Computer`
+        winModal(winner)
+        newGameStart();
+      }
+      let afterCount = 1
+      const interval2 = setInterval(() => {
+        afterCount--;
+        if (afterCount < 0) {
+          clearInterval(interval2);
+          newRound();
+          enableButtons();
+        }
+      }, 600)
+    }
+
+  }, 500);
+}
+function disableButtons() {
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach(function (button) {
+    button.disabled = true;
+  });
+}
+function enableButtons() {
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach(function (button) {
+    button.disabled = false;
+  });
+}
+function newRound() {
+  changeImage(playerPic, resetPic);
+  changeImage(compPic, resetPic);
+  const timer = document.querySelector("#countDown");
+  timer.textContent = `VS`
+}
+function newGameStart() {
+  humanScore = 0;
+  computerScore = 0;
+  updateScore();
+  changeImage(playerPic, resetPic);
+  changeImage(compPic, resetPic);
+  const timer = document.querySelector("#countDown");
+  timer.textContent = `VS`
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When function called, open the modal and names the winner
+function winModal(winner) {
+  modal.style.display = "block";
+  const text = document.getElementById("winText")
+  text.textContent = `${winner} has won the game!`
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
